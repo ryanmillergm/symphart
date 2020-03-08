793214d72c06c046690c7f4afb511f3079457f53
Ryan Miller <ryanmillergm@gmail.com>
2020-03-08 15:32:05 -0600

    Updates articles controller to use annotation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,15 @@
   namespace App\Controller;
 
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
   Class ArticleController {
+    /**
+     * @Route("/", name="article_list")
+     * @Method({"GET"})
+     */
+
     public function index() {
       return new Response('<html><body>Hello</body></html>');
     }
