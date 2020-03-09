3f5334180de68befa3ac7f302ffd7bc37d723713
Ryan Miller <ryanmillergm@gmail.com>
2020-03-09 16:16:04 -0600

    Adds ToDo entity

---
+++ src/Entity/ToDo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ToDoRepository")
+ */
+class ToDo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+    * @ORM\Column(type="text")
+    */
+    private $body;
+
+    //Getters and Setters
+
+
+    // public function getId(): ?int
+    // {
+    //     return $this->id;
+    // }
+}
