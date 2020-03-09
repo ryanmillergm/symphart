cc0ee3cffa29a9f78760e2585a04f54af642b94f
Ryan Miller <ryanmillergm@gmail.com>
2020-03-09 16:51:03 -0600

    Updates todo index function to retrieve and render all todos

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -1,6 +1,8 @@
 <?php
   namespace App\Controller;
 
+  use App\Entity\ToDo;
+
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -8,15 +10,31 @@
 
   class ToDoController extends AbstractController {
     /**
-     * @Route("/todos")
+     * @Route("/todo")
      * @Method({"GET"})
      */
 
     public function index() {
 
-      $todo_list = ['to-do one', 'to-do two', 'to-do three'];
+      $todo_list = $this->getDoctrine()->getRepository(ToDo::class)->findAll();
 
       return $this->render('todos/index.html.twig', array('todo_list' => $todo_list));
     }
+
+    /**
+    *@Route("/todo/save")
+    */
+    public function save(){
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $todo_list = new ToDo();
+      $todo_list->setTitle('To-Do 2: Clean bathroom.');
+      $todo_list->setBody('I will clean sinks, toilet, shower and tub');
+
+      $entityManager->persist($todo_list);
+
+      $entityManager->flush();
+
+      return new Response('Saved an To-Do List with the id of '.$todo_list->getId());
+    }
   }
-?>
