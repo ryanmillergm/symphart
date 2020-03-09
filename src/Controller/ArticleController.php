4bec582b758eceb2864c0261a4141b408ac4f379
Ryan Miller <ryanmillergm@gmail.com>
2020-03-09 17:45:07 -0600

    Adds new article function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,14 @@
   use App\Entity\Article;
 
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
   class ArticleController extends AbstractController {
     /**
@@ -22,15 +27,40 @@
       return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
-    /**
-     * @Route("/article/{id}", name="article_show")
-     * @Method({"GET"})
+     /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
      */
-     public function show($id) {
-       $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-       return $this->render('articles/show.html.twig', array('article' => $article ));
+     public function new(Request $request) {
+       $article = new Article();
+
+       $form = $this->createFormBuilder($article)
+       ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+       ->add('body', TextareaType::class, array(
+         'required' => false,
+         'attr' => array('class' => 'form-control')
+       ))
+       ->add('save', SubmitType::class, array(
+         'label' => 'Create',
+         'attr' => array('class' => 'btn btn-primary mt-3')
+       ))
+       ->getForm();
+
+       return $this->render('articles/new.html.twig', array('form' => $form->createView()
+     ));
      }
+
+
+     /**
+      * @Route("/article/{id}", name="article_show")
+      * @Method({"GET"})
+      */
+      public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array('article' => $article ));
+      }
 
     // /**
     // *@Route("/article/save")
