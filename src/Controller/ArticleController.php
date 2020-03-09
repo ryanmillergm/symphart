8b149b877773ce692e180d09abb577def1fed8a6
Ryan Miller <ryanmillergm@gmail.com>
2020-03-09 13:31:38 -0600

    Updates function save to save article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,8 @@
 <?php
   namespace App\Controller;
 
+  use App\Entity\Article;
+
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -18,6 +20,23 @@
       $articles = ['Article 1', 'Article 2'];
 
       return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+    *@Route("/article/save")
+    */
+    public function save(){
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $article = new Article();
+      $article->setTitle('Article One');
+      $article->setBody('This is the body for article one');
+
+      $entityManager->persist($article);
+
+      $entityManager->flush();
+
+      return new Response('Saved an article with the id of '.$article->getId());
     }
   }
 ?>
