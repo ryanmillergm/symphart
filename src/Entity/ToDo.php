ba8da82a370348cf1235e0a07948293093f14f48
Ryan Miller <ryanmillergm@gmail.com>
2020-03-09 16:26:01 -0600

    Updates todo entity to get and set title and body

--- src/Entity/ToDo.php
+++ src/Entity/ToDo.php
@@ -29,9 +29,23 @@ class ToDo
 
     //Getters and Setters
 
+    public function getId() {
+      return $this->id;
+    }
 
-    // public function getId(): ?int
-    // {
-    //     return $this->id;
-    // }
+    public function getTitle() {
+      return $this->title;
+    }
+
+    public function setTitle($title) {
+      $this->title = $title;
+    }
+
+    public function getBody() {
+      return $this->body;
+    }
+
+    public function setBody($body) {
+      $this->body = $body;
+    }
 }
